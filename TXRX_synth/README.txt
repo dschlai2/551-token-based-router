61f16b16c23d41cfdc41f9b3ab9b95c55ec67638
m1k3st4rr <m1k3st4rr@gmail.com>
2011-04-04 20:29:30 +0000

    added directory for synthesized code

---
+++ TXRX_synth/README.txt
@@ -0,0 +1,8 @@
+Mike Starr
+Patrick Hagen
+Rob Frank
+
+
+Note:
+	trans_protocol and receive_protocol are submodules of their respective
+	units.  These handle the transmission along the S_Data line.
---
+++ TXRX_synth/TX_datavectors.v
@@ -0,0 +1,9 @@
+/* Token: */
+parameter = num_Tests;
+reg [54:0] inVector [num_Tests] = 	  
+	   '{'h111xx_xxxxxxxxxx_xxxxxxxxxx_xxxxxxxxxx_xxxxxxxxxx_xxxxxxxxxx,
+             'h000xx_xxxxxxxxxx_xxxxxxxxxx_xxxxxxxxxx_xxxxxxxxxx_xxxxxxxxxx,
+             'h011xx_xxxxxxxxxx_xxxxxxxxxx_xxxxxxxxxx_xxxxxxxxxx_xxxxxxxxxx,
+             'h01012_3456789012_3456789012_1111111111_2222222222_3333333333,
+             'h00199_8877665544_3322110099_8887776665_5544433322_1110009999}
+  
