3a9b8853aa60b827271fe81d48621e0a9d61f9c6
m1k3st4rr@gmail.com <m1k3st4rr@gmail.com@caad254a-15ab-db0f-ba08-885804b149a7>
2011-04-05 00:47:44 +0000

    IT FUCKING WORKS IT WORKSsvn statussvn statussvn statussvn statussvn status!

--- trunk/TXRX_synth/trans_protocol.v
+++ trunk/TXRX_synth/trans_protocol.v
@@ -6,19 +6,22 @@ module trans_protocol(TX_Data,
    input 	start, rst, clk;
    output reg 	ready, S_Data;
       
-   parameter sz_START_SEQ = 5'd6,
-	       sz_DATA =  6'd55;
+   parameter [4:0] sz_START_SEQ = 5'd6;
+   parameter [5:0] sz_DATA =  6'd55;
       
-   parameter START_SEQ = 6'b01_1111;
-   parameter START = 3'd0,
-		S_SEQ = 3'd1, 
-		TRANSMIT = 3'd4,
-		DONE = 3'd5,
-		WAIT = 3'd6;
+   parameter [5:0] START_SEQ = 6'b01_1111;
+   parameter [2:0] START = 3'd0;
+   parameter [2:0] S_SEQ = 3'd1;
+   parameter [2:0] TRANSMIT = 3'd4;
+   parameter [2:0] DONE = 3'd5;
+   parameter [2:0] WAIT = 3'd6;
 
    reg 	     next_ready, next_S_Data;
    reg [3:0] state, next_state;
    reg [5:0] next_counter, counter;
+   wire [5:0] counter_1;
+
+   assign counter_1 = counter -1;
 
    /* State transitions */
    always @(posedge clk, posedge rst) begin
@@ -26,6 +29,7 @@ module trans_protocol(TX_Data,
 	 state <= WAIT;
 	 S_Data <= 1'b0;
 	 ready <= 1'b0;
+	 counter <= 6'b1;
       end else begin
 	 S_Data <= next_S_Data;
 	 state <= next_state;
@@ -44,7 +48,7 @@ module trans_protocol(TX_Data,
 	     next_counter = sz_START_SEQ;
 	  end else begin
 	     next_state = WAIT;
-	     next_counter = 6'bx;
+	     next_counter = 6'b1;
 	  end
 
 	/* Transmitting start_seq */
@@ -68,19 +72,19 @@ module trans_protocol(TX_Data,
 	     next_counter = counter - 1;
 	  end else begin
 	     next_state = DONE;
-	     next_counter = 6'bx;
+	     next_counter = 6'b1;
 	  end
 
 
       /* Return to WAIT */
 	DONE: begin
 	   next_state = WAIT;
-	   next_counter = 6'bx;
+	   next_counter = 6'b1;
 	end
 	
 	default: begin
 	   next_state = WAIT;
-	   next_counter = 6'bx;
+	   next_counter = 6'b1;
 	end
       endcase // case (state)
    end // always @ (state)
@@ -90,12 +94,18 @@ module trans_protocol(TX_Data,
    always @(state, counter) begin
       case (state)
 	 START: begin
-	    next_S_Data = START_SEQ[counter-1];
+	    if (counter > 0) begin
+	      next_S_Data = START_SEQ[counter_1];
+	    end else
+	      next_S_Data = 1'b1;
 	    next_ready = 1'b0;
 	 end
 
 	 TRANSMIT: begin
-	    next_S_Data = TX_Data[counter-1];
+	    if (counter > 0) begin
+	      next_S_Data = TX_Data[counter_1];
+	    end else
+	      next_S_Data = 1'b1;
 	    next_ready = 1'b0;
 	 end	
 
@@ -110,8 +120,8 @@ module trans_protocol(TX_Data,
 	end
 	
 	default: begin
-	   next_S_Data = 1'bx;
-	   next_ready = 1'bx;
+	   next_S_Data = 1'b1;
+	   next_ready = 1'b0;
 	end
 	
       endcase // case state
