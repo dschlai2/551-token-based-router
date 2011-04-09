191e0d0da8d145f3a7c67df286849692c336d49f
Patrick.William.Hagen <Patrick.William.Hagen@caad254a-15ab-db0f-ba08-885804b149a7>
2011-04-09 02:27:31 +0000

    updated checksum encoder

--- routerCore/checksumEncoder.v
+++ routerCore/checksumEncoder.v
@@ -2,10 +2,10 @@ module checksum_encoder(input [23:0] payload, output reg [27:0] out);
 
 	integer i;
 	reg [5:0] count;
-	parameter load = 24;	
+	parameter [5:0] load = 6'd24;	
 always@(*) begin
 	count = 6'b0;
-	for(i = 0; i < 24; i = i + 1) begin
+	for(i = 0; i < load; i = i + 1) begin
 		if(payload[i] == 1'b1) begin
 			count = count + 1'b1;
 		end
