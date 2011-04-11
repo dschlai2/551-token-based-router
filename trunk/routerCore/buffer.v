1cf1146caccbd5e4aeecb06740000a36fa729ea3
m1k3st4rr@gmail.com <m1k3st4rr@gmail.com@caad254a-15ab-db0f-ba08-885804b149a7>
2011-04-11 22:24:59 +0000

    modified buffer.  Select now chooses between holding its current value, selecting the line from RX (new), and selecting the line from the processor node.

--- trunk/routerCore/buffer.v
+++ trunk/routerCore/buffer.v
@@ -1,22 +1,28 @@
-module buffer(new_value, buffer_select, clk, rst, out);
+module buffer(from_processor, from_rx, buffer_select, clk, rst, out);
     
-    input [54:0] new_value;
-    input clk, rst, buffer_select;
-    output reg [54:0] out;
-    
-    always@(posedge clk, posedge rst)begin
-        // Reset
-        if(rst)begin
-           out <= 55'b0;
-        end
-        
-        // Otherwise
-        else begin
-        if(buffer_select == 1'b1)
-            out <= new_value;
-        else
-            out <= out;
-        end // else
-        
-    end // Always
+   input [54:0] from_processor, from_rx;
+   input 	clk, rst;
+   input [1:0] 	buffer_select;
+   output reg [54:0] out;
+
+   parameter [1:0] HOLD = 0;
+   parameter [1:0] PROC = 1;
+   parameter [1:0] RX   = 2;
+   
+   always@(posedge clk, posedge rst)begin
+      // Reset
+      if(rst)begin
+         out <= 55'b0;
+      end
+      
+      // Otherwise
+      else begin
+	 case (buffer_select) /* select data line */
+	   HOLD: out <= out;
+	   PROC: out <= from_processor;
+	   RX:   out <= from_rx;
+	   default: 55'b0;
+      end // else
+      
+   end // Always
 endmodule
