61718d8457d28f1d86f3be50bf56494b17f1c7c6
Patrick.William.Hagen <Patrick.William.Hagen@caad254a-15ab-db0f-ba08-885804b149a7>
2011-04-12 02:45:02 +0000

    Fixed Master mux

--- trunk/routerCore/master_mux.v
+++ trunk/routerCore/master_mux.v
@@ -5,15 +5,17 @@ module master_mux(input [54:0] token, nack, ack, buffer, data,input [2:0] TX_dat
 always@(TX_data_select) begin 
 	case(TX_data_select)
 
-	3'd0: out = data;
+	3'd2: out = data;
 
-	3'd1: out = buffer;
+	3'd4: out = buffer;
 
-	3'd2: out = token;
+	3'd3: out = token;
 
-	3'd3: out = nack;
+	3'd1: out = nack;
 
-	3'd4: out = ack;
+	3'd0: out = ack;
+
+	default: out = 55'b0;
 
 	endcase
 end
