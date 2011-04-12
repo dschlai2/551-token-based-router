33911e246ccdd5e2775e457fd4c13d2e46b0446d
m1k3st4rr <m1k3st4rr@gmail.com>
2011-04-12 01:41:39 +0000

    removed project files

--- routerCore/work/decoder_3of6/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity decoder_3of6 is
-    port(
-        encoded         : in     vl_logic_vector(47 downto 0);
-        payload         : out    vl_logic_vector(23 downto 0);
-        error_3of6      : out    vl_logic
-    );
-end decoder_3of6;
--- routerCore/work/six_bit_checker/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity six_bit_checker is
-    port(
-        \in\            : in     vl_logic_vector(5 downto 0);
-        error           : out    vl_logic
-    );
-end six_bit_checker;
