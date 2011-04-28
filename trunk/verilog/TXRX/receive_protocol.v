b18780815a66f5cf20b044c6ddba4854db9dfbac
m1k3st4rr@gmail.com <m1k3st4rr@gmail.com@caad254a-15ab-db0f-ba08-885804b149a7>
2011-04-28 01:19:55 +0000

    updated working copies of trans/recv in verilog folder

--- trunk/verilog/TXRX/receive_protocol.v
+++ trunk/verilog/TXRX/receive_protocol.v
@@ -2,46 +2,75 @@ module receive_protocol(input S_Data, clk, rst,
     output reg [54:0] packet,
     output reg 	      ready);
 
-reg [2:0] state, next_state;
-reg next_in;
-reg [5:0] start_seq;
-reg [6:0] next_counter, counter; 
+
+reg [2:0] 	      state, next_state;
+reg 	  next_in;
+reg [5:0] start_seq, next_start_seq;
+reg [6:0] next_counter, counter;
+reg [54:0] next_packet;
 
 parameter WAIT = 3'b001, READ = 3'b010 , DONE = 3'b100;
 
 always@(posedge clk, negedge rst) begin
-
 	if(!rst) begin
-		state <= 3'b001;
-		start_seq <= 6'b111111;
+	   state <= WAIT;
+	   start_seq <= 6'b111111;
+	   counter <= 7'd54;
+	   packet <= 55'b0;
 	end
-	else
-		state <= next_state;
-		counter <= next_counter;
+	else begin
+	   state <= next_state;
+	   counter <= next_counter;
+	   start_seq <= next_start_seq;
+	   packet <= next_packet;
+	end
 end
 
 always@(*) begin
 
 	case(state) 
-
-	WAIT: if(start_seq == 6'b011111) begin
+	  WAIT: begin
+	     next_counter = 7'd54;
+	     next_start_seq = {start_seq[4:0],S_Data};
+	     if(start_seq == 6'b011111) begin
 		next_state = READ;
-		next_counter = 7'd55;
-		packet[next_counter-1] = S_Data;
-				end
-	      else
+		next_packet = packet;
+		next_packet[counter] = S_Data;
+		next_counter = counter -1;
+	     end else begin
 		next_state = WAIT;
-
-	READ: if(next_counter == 0)
-		next_state = DONE;
-	      else begin
-		next_state = READ;
-		next_counter = counter - 1;
-		packet[next_counter-1] = S_Data;
+		next_packet = packet;
 	     end
+	  end
+	  
+	  READ:  begin
+	     next_start_seq = 6'b111111;
+	     if(counter == 0) begin
+		next_state = DONE;
+		next_counter = 7'd20;
+		next_packet = packet;
+		next_packet[counter] = S_Data;
+	     end else begin
+		next_state = READ;
+		next_packet = packet;
+		next_packet[counter] = S_Data;
+		next_counter = counter-1;
+	     end
+	  end
+	  
+	  DONE: begin
+	     next_counter = 7'd54;
+	     next_state = WAIT;
+	     next_start_seq = 6'b111111;
+	     next_packet = packet;
+	  end
 
-	DONE: next_state = WAIT;
-
+	  default: begin
+	     next_counter = 7'd54;
+	     next_state = WAIT;
+	     next_start_seq = 6'b111111;
+	     next_packet = packet;
+	  end
 	endcase
 end
 		
@@ -49,16 +78,10 @@ always@(state) begin
 
 	if(state == DONE)begin
 		ready = 1'b1;
-		start_seq <= 6'b111111;
 	end else
 		ready = 1'b0;
 end
 
-
-//Shifts start sequence over by 1 and adds new input
-   always@(posedge clk) begin 
-      start_seq <= {start_seq[4:0],S_Data};
-   end
 
 endmodule
 	
